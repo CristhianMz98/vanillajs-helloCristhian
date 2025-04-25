0ea142e3ec234d866538041fe4303a86f9b87e89
CristhianMz98 <cmtz2503@gmail.com>
2025-04-25 19:01:15 +0000

    nombres de dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon']; 
+let extensions = ['.com', '.es', '.org'];
+
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let e of extensions) {
+        console.log(p + a + n + e);
+      }    
+    }    
+  }
+}
